8d64100da69b487df127125be5813caae5c4c1fb
KseniaShkolnikova <isip_k.v.shkolnikova@mpt.ru>
2023-11-23 20:35:59 +0300

    hfjd

---
+++ тест/Class2.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace тест
+{
+    public class tablica
+    {
+        public string name;
+        public double vminutu;
+        public double vsec;
+        public static void zapictablica(string name, double vsrc, double k)
+        {
+            string json = "";
+            List<tablica> tablicas = new List<tablica>();
+            tablica cpicki = new tablica();
+            cpicki.name = name;
+            cpicki.vsec = vsrc;
+            cpicki.vminutu = k;
+            tablicas.Add(cpicki);
+            string ajson = File.ReadAllText("C:\\Users\\user1\\Desktop\\resul.json");
+            List<tablica> vivodы = JsonConvert.DeserializeObject<List<tablica>>(ajson);
+            try
+            {
+                vivodы.Add(cpicki);
+                json = JsonConvert.SerializeObject(vivodы);
+            }
+            catch (Exception)
+            {
+                json = JsonConvert.SerializeObject(tablicas);
+            }
+            finally
+            {
+
+                File.WriteAllText("C:\\Users\\user1\\Desktop\\resul.json", json);
+            }
+        }
+        public static void tablicar()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine("                                                   Таблица рекордов                                                    ");
+            Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------");
+            string json = File.ReadAllText("C:\\Users\\user1\\Desktop\\resul.json");
+            List<tablica> vivoda = JsonConvert.DeserializeObject<List<tablica>>(json);
+            foreach (var cpicki in vivoda)
+            {
+                Console.WriteLine("Имя                 " + cpicki.name);
+                Console.WriteLine("Скорость в минуту   " + cpicki.vminutu);
+                Console.WriteLine("Скорость в секунду  " + cpicki.vsec);
+                Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+            }
+            Console.WriteLine("Если хотите повторить игру - нажмите Enter.Если хотите выйти - Escape");
+            Thread.Sleep(1000);
+            ConsoleKeyInfo knopk3 = Console.ReadKey(true);
+            if (knopk3.Key == ConsoleKey.Enter)
+            {
+
+            }
+            else if (knopk3.Key == ConsoleKey.Escape)
+            {
+                Console.WriteLine("Пока пока");
+                Environment.Exit(0);
+            }
+        }
+    }
+    
+}
---
+++ тест/Program.cs
@@ -0,0 +1,160 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Diagnostics;
+using System.Runtime.ConstrainedExecution;
+using System.Threading;
+
+namespace тест
+{
+    internal class Program
+    {
+        public static string name ;
+        public static double seconds;
+        public static double k;
+        public static double vsrc;
+        public static double o;
+        public static char[] vvod;
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo knopk;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Clear(); ;
+                Console.WriteLine("Введите ваше имя: ");
+                name = Console.ReadLine();
+                Console.Clear();
+                vvod = Class1.s.ToCharArray();
+                Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+                foreach (char item in vvod)
+                {
+                    Console.Write(item);
+                }
+                Console.WriteLine(' ');
+                Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+                Console.WriteLine("Когда будете готовы - нажмите Enter. Для выхода - Escape");
+                knopk = Console.ReadKey();
+                Console.Clear();
+                if (knopk.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+                    foreach (char item in vvod)
+                    {
+                        Console.Write(item);
+                    }
+                    Console.WriteLine("");
+                    Console.WriteLine("------------------------------------------------------------------------------------------------------------------------");
+                    k = 0;
+                    o = 0;
+
+                    vrema();
+                }
+            } while (knopk.Key != ConsoleKey.Escape);
+            Console.WriteLine("gПока пока");
+            Environment.Exit(0);
+        }
+        static void vrema()
+        {
+            seconds = 0;
+            int minutes = 1;
+            Thread vremi = new Thread(_ =>
+            {
+                do
+                {
+                    try
+                    {
+                        double der;
+                        der = k + o;
+                        Console.SetCursorPosition(0, 10);
+                        Console.Write($"{minutes}:{seconds.ToString().PadLeft(2, '0')} ");
+                        Thread.Sleep(1000);
+                        if (der == 449)
+                        {
+                            break;
+                        }
+                        if (seconds == 0 && minutes == 1)
+                        {
+                            minutes--;
+                            seconds = 60;
+                        }
+                        seconds--;
+                    }
+                    catch (System.Threading.ThreadInterruptedException)
+                    {
+                        break;
+                    }
+                } while ( seconds >= 0);
+                Console.SetCursorPosition(0, 10);
+                Console.WriteLine("Стоп!");
+                
+            });
+            vremi.Start();
+            text();
+            Thread.Sleep(2000);
+            ConsoleKeyInfo knopk2 = Console.ReadKey(true);          
+            if (knopk2.Key == ConsoleKey.Escape)
+            {
+                Console.WriteLine("Пока пока");
+                Environment.Exit(0);
+            }
+            else 
+            {
+                tablica.zapictablica(name,vsrc,k);
+                tablica.tablicar();                
+            }
+
+        }
+        internal static void text()
+        {
+            int j = 1;
+            int h = 0;
+            int i=0;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                try
+                {
+                    ConsoleKeyInfo uservvodit = Console.ReadKey(true);
+                    char a = uservvodit.KeyChar;
+                    Console.SetCursorPosition(h, j);
+                    Console.WriteLine(a);
+                    if (a == vvod[i])
+                    {
+                        k++;
+                    }
+                    else if (a != vvod[i])
+                    {
+                        o++;
+                    }
+                    h++;
+                }
+                catch (Exception)
+                {
+                    h = 0;
+                    j++;
+                    ConsoleKeyInfo uservvodit = Console.ReadKey(true);
+                    char a = uservvodit.KeyChar;
+                    Console.SetCursorPosition(h, j);
+                    Console.WriteLine(a);
+                    if (a == vvod[i])
+                    {
+                        k++;
+                    }
+                    else if (a != vvod[i])
+                    {
+                        o++;
+                    }
+                }
+                finally
+                {
+                    i++;
+                }
+                
+            } while (k + o < 449 && seconds > 0);
+            vsrc = k / 60;
+        }
+        
+
+           
+    }
+}
